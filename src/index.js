35ca826cded841831d04cfe9150a53c7a066205c
Ivan Driuk <veytarn@gmail.com>
2018-01-20 22:27:06 +0200

    wrap router with redux provider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise'
+
 import './style/index.css';
-import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import reducers from './reducers';
+
+import App from './components/App/App';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
 ReactDOM.render(
-  <BrowserRouter>
-      <Switch>
-        <Route path="/" component={App} />
-      </Switch>
-  </BrowserRouter>,
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+        <Switch>
+          <Route path="/" component={App} />
+        </Switch>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
