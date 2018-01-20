34cb4222bc865ba1265c7dac336b5fe501d6cd1f
Ivan Driuk <veytarn@gmail.com>
2018-01-20 22:11:44 +0200

    put App in separate folder, wrap App with router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './style/index.css';
+import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
