d1a281df89fa9b158040ca4389c77207ac452a14
Ivan Driuk <veytarn@gmail.com>
2018-01-21 17:14:04 +0200

    add header navigation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,30 @@ class App extends Component {
   render() {
     return (
       <div className="y-container homepage-hero">
-        Welcome Yalp
+
+        {/* Top menu */}
+        <ul className="header-nav">
+          <li className="header-nav_item">
+            <a className="header-nav_link">Write a Review</a>
+          </li>
+          <li className="header-nav_item">
+            <a className="header-nav_link">Events</a>
+          </li>
+          <li className="header-nav_item">
+            <a className="header-nav_link">Talk</a>
+          </li>
+        </ul>
+
+        {/* Registration buttons */}
+        <ul>
+          <li>Log In</li>
+          <li>Sign Up</li>
+        </ul>
+
+        <h1 className="homepage-hero_logo">
+          <a href="/">Yelp</a>
+        </h1>
+
       </div>
     );
   }
