ae7ebc070e1b7924ad4975c97bbe21a3d31fad9f
Ivan Driuk <veytarn@gmail.com>
2018-01-21 22:08:42 +0200

    add account buttons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,7 +10,6 @@ class App extends Component {
             <div className="arrange arrange--18">
               <div className="arrange_unit arrange_unit--fill">
                 <div className="hero-header_nav hero-header_nav--main nowrap">
-                  {/* Top menu */}
                   <ul className="header-nav">
                     <li className="header-nav_item">
                       <a className="header-nav_link">Write a Review</a>
@@ -22,17 +21,17 @@ class App extends Component {
                       <a className="header-nav_link">Talk</a>
                     </li>
                   </ul>
-
-                  {/* Registration buttons
-                  <ul>
-                    <li>Log In</li>
-                    <li>Sign Up</li>
-                  </ul>
-
-                  <h1 className="homepage-hero_logo">
-                    <a href="/">Yelp</a>
-                  </h1>*/}
                 </div>
+              </div>
+              <div className="arrange_unit nowrap">
+                <ul className="header-nav hero-header_nav main-header_account">
+                  <li className="header-nav_item u-space-r2">
+                    <a className="header-nav_link header-nav_link--log-in">Log In</a>
+                  </li>
+                  <li className="header-nav_item u-space-r0">
+                    <a className="ybtn ybtn--primary header-nav_button nowrap">Sign Up</a>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
