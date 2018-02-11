0cae69dd31c3307451dddfb96df8437ec08a7a7e
Ivan Driuk <veytarn@gmail.com>
2018-02-11 17:56:45 +0200

    fix FeedThreePhoto test

--- src/components/FeedThreePhoto/FeedThreePhoto.test.js
+++ src/components/FeedThreePhoto/FeedThreePhoto.test.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import FeedThreePhoto from './FeedThreePhoto';
 
+const data = {
+  avatar: "avatar_2",
+  big_photo: "feed_photo_big_1",
+  photos: ["feed_photo_1", "feed_photo_2"],
+  author: "Zak R.",
+  photos_count: "Added 5 photos",
+  biz_name: "Alexander’s Steakhouse",
+  show_all: "Show all 5 photos"
+}
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
 
-  ReactDOM.render(<FeedThreePhoto />, div);
+  ReactDOM.render(<FeedThreePhoto {...data} />, div);
 })
